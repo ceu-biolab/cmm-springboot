74ef685ed32d054532a580fd8aa5c3263a5d15bf
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-03-05 14:31:14 +0100

    updates to try the endpoint

--- src/main/java/com/example/myapp/repository/FormDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.repository;
-
-import com.example.myapp.model.FormData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormDataRepository extends JpaRepository<FormData, Long> {
-}
