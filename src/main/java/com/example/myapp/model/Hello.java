961c6ba3fcedd6e89c9f2d4e70752da2b63dba38
Pablo <pabloqwe1000@gmail.com>
2025-03-13 19:55:38 +0100

    Hello world rule

---
+++ src/main/java/com/example/myapp/model/Hello.java
@@ -0,0 +1,24 @@
+package com.example.myapp.model;
+
+public class Hello {
+    private String val;
+
+    public Hello(String val) {
+        super();
+        this.val = val;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello [");
+        if (val != null)
+            builder.append("val=").append(val);
+        builder.append("]");
+        return builder.toString();
+    }
+}
