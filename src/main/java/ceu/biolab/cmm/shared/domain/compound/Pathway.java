3b19453022274b1634ab347c3d73868f033c1189
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-05-07 23:18:14 +0200

    added equals to pathway

--- src/main/java/ceu/biolab/cmm/shared/domain/compound/Pathway.java
+++ src/main/java/ceu/biolab/cmm/shared/domain/compound/Pathway.java
@@ -1,5 +1,7 @@
 package ceu.biolab.cmm.shared.domain.compound;
 
+import java.util.Objects;
+
 public class Pathway {
     private Integer pathwayId;
     private String pathwayName;
@@ -55,6 +57,19 @@ public class Pathway {
 
     public void setPathwayMap(String pathwayMap) {
         this.pathwayMap = pathwayMap;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pathway pathway = (Pathway) o;
+        return Objects.equals(pathwayId, pathway.pathwayId) && Objects.equals(pathwayName, pathway.pathwayName) && Objects.equals(pathwayMap, pathway.pathwayMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pathwayId, pathwayName, pathwayMap);
     }
 
     @Override
